714900ba935d4fe56aad7a5b1cf34b905877d01a
deeptanshunayak <deeptanshunayak@gmail.com>
2024-03-28 17:22:01 +0530

    commit

---
+++ src/Routes.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+import LandingPage from "pages/LandingPage";
+import AboutUs from "pages/AboutUs";
+import Listing from "pages/Listing";
+import ListingMapView from "pages/ListingMapView";
+import PropertyDetails from "pages/PropertyDetails";
+
+const ProjectRoutes = () => {
+  let element = useRoutes([
+    { path: "dhiwise-dashboard", element: <Home /> },
+    { path: "*", element: <NotFound /> },
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/aboutus",
+      element: <AboutUs />,
+    },
+    {
+      path: "/listing",
+      element: <Listing />,
+    },
+    {
+      path: "/listingmapview",
+      element: <ListingMapView />,
+    },
+    {
+      path: "/propertydetails",
+      element: <PropertyDetails />,
+    },
+  ]);
+
+  return element;
+};
+
+export default ProjectRoutes;
---
+++ src/components/index.jsx
@@ -0,0 +1,12 @@
+import { Img } from "./Img";
+import { Text } from "./Text";
+import { Heading } from "./Heading";
+import { Button } from "./Button";
+import { Input } from "./Input";
+import { GoogleMap } from "./GoogleMap";
+import { SelectBox } from "./SelectBox";
+import { RadioGroup } from "./RadioGroup";
+import { TextArea } from "./TextArea";
+import { Radio } from "./Radio";
+import { RatingBar } from "./RatingBar";
+export { Img, Text, Heading, Button, Input, GoogleMap, SelectBox, RadioGroup, TextArea, Radio, RatingBar };
